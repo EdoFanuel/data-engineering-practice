a9337e4f59283ca1ae9e87bfe6c24398ee3b1bf4
Edo Fanuel Siswanto <edofanuel@gmail.com>
2024-05-05 22:02:25 -0400

    Exercise 4

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,10 +1,25 @@
-import boto3
-
+import os
+import json
+import pandas as pd
 
 def main():
-    # your code here
-    pass
+    data = []
+    for root, _, files in os.walk('data'):
+        for file in files:
+            filepath = f'{root}/{file}'
+            print(f'Processing {filepath}')
+            try:
+                data.append(parse_json(filepath))
+            except:
+                pass
+    df = pd.json_normalize(data)
+    df.to_csv('answer.csv', index=False)
 
+
+def parse_json(filepath):
+    with open(filepath) as json_file:
+        data = json.load(json_file)
+        return data
 
 if __name__ == "__main__":
     main()
